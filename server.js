f1c76389ee502ae17138cb2dec6d38f499517622
MatheusRodrigues <matheuswillianrs33@gmail.com>
2025-05-19 14:21:05 -0300

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+// Middleware para servir arquivos estáticos
+app.use(express.static(__dirname))
+
+// Body-parser para ler dados de formulários
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// Inicializando servidor
+const PORT = 8080
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`)
+})
